b50ff6eea7158679499fd64c915362598c410f7d
Yelyzaveta2004 <ucomfy50@gmail.com>
2024-01-20 17:26:19 +0200

    Зробила домашнє завдання

--- js/task-2.js
+++ js/task-2.js
@@ -1,7 +1,18 @@
- function getShippingMessage(country, price, deliveryFee) {
-  const totalPrice = price + deliveryFee;
-  return `Shipping to ${country} will cost ${totalPrice} credits`;
+ function formatMessage(message, maxLength) {
+  // Отримуєм довжину рядка
+  const messageLength = message.length;
+
+  // Якщо довжина рядка дорівнює або менша за maxLength, то повертаєм початковий рядок без змін
+  if (messageLength <= maxLength) {
+    return message;
+  }
+
+  // В іншому випадку обрізаю рядок до maxLength символів і додаю трикрапку в кінці
+  return message.slice(0, maxLength - 3) + "...";
 }
-console.log(getShippingMessage("Australia", 120, 50)); // "Shipping to Australia will cost 170 credits"
-console.log(getShippingMessage("Germany", 80, 20)); // "Shipping to Germany will cost 100 credits"
-console.log(getShippingMessage("Sweden", 100, 20)); // "Shipping to Sweden will cost 120 credits"
+console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
+console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
+console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis..."
+console.log(formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis..."
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); // "Nunc sed turpis a felis in nunc fringilla"
